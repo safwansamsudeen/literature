35459a1049d52301f0803116128f5b18edc23d0e
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2024-06-26 21:15:22 +0530

    basic app: cards, people, calling, and turns

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,56 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { writable } from 'svelte/store';
+
+export const card = (x) => `https://unpkg.com/cardsJS/dist/cards/${x}.svg`;
+export const NUMBERS = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "K",
+    "Q",
+    "J",
+    "A"
+];
+export const SUITS = ["S", "H", "D", "C"];
+
+let teams;
+export const turn = writable(1);
+
+export function shuffle(cards, ids) {
+    for (let i = ids.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [ids[i], ids[j]] = [ids[j], ids[i]];
+    }
+    const container = document.getElementById("card-container");
+    container.innerHTML = "";
+    ids.forEach((id) => container.appendChild(cards[id]));
+    return true;
+}
+
+export function assignTeams(cards, ids) {
+    teams = { 1: [], 2: [], 3: [], 4: [], 5: [], 6: [] };
+
+    for (let t = 1; t <= 6; t++) {
+        teams[t] = ids.slice((t - 1) * 9, t * 9);
+    }
+
+    for (let team in teams) {
+        teams[team].forEach((id) =>
+            document.getElementById("team-" + team).appendChild(cards[id]),
+        );
+    }
+}
+
+export function call(cards, team1, team2, id) {
+    if (!teams[team2].includes(id)) turn.set(team2);
+
+    teams[team1].push(id);
+    teams[team2].splice(teams[team2].indexOf(id), 1);
+
+    document.getElementById("team-" + team1).appendChild(cards[id]);
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,71 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from "svelte";
+    import {
+        call,
+        card,
+        shuffle,
+        assignTeams,
+        SUITS,
+        NUMBERS,
+        turn,
+    } from "$lib/index";
+    // let turn = 1;
+
+    let cards = {};
+    let ids = [];
+
+    onMount(() => {
+        const container = document.getElementById("card-container");
+        SUITS.forEach((suit) => {
+            NUMBERS.forEach((n) => {
+                let i = document.createElement("img");
+                i.src = card(n + suit);
+                i.classList.add("card");
+                i.id = n + suit;
+                cards[n + suit] = i;
+                ids.push(n + suit);
+                container.appendChild(i);
+            });
+        });
+
+        shuffle(cards, ids);
+        assignTeams(cards, ids);
+    });
+</script>
+
+<div>
+    <button
+        on:click={() => call(cards, $turn, prompt("Callee:"), prompt("Card:"))}
+        >Call</button
+    >
+    <button on:click={() => shuffle(cards, ids) && assignTeams(cards, ids)}
+        >Shuffle</button
+    >
+</div>
+
+<div id="card-container"></div>
+{#each [1, 2, 3, 4, 5, 6] as i}
+    <div class="team-block">
+        <h2>Team {i}</h2>
+        <div
+            class="hand hhand-compact {$turn == i ? 'active-hand' : ''}"
+            id="team-{i}"
+        ></div>
+    </div>
+{/each}
+
+<style>
+    .team-block {
+        width: 48%;
+        display: inline-block;
+    }
+
+    .active-hand {
+        border: 1px solid black;
+        border-radius: 3px;
+    }
+
+    .hand {
+        padding: 10px 40px;
+    }
+</style>
